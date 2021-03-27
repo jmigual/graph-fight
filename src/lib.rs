7a040ca22bb8e15cf5f14ef3d76e38b83afc866a
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2021-03-27 19:39:16 +0100

    Start implementing evaluator

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use std::vec::Vec;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -8,12 +9,54 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+struct Point {
+    x: f32,
+    y: f32
+}
+
+struct Player {
+    pos: Point,
+    radius: f32
+}
+
+struct Circle {
+    pos: Point,
+    radius: f32
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, graph-fight!");
+pub struct GraphFight {
+    playerA: Vec<Player>,
+    playerB: Vec<Player>,
+    obstacles: Vec<Circle>,
+    x_range: (f32, f32),
+    y_range: (f32, f32)
 }
+
+#[wasm_bindgen]
+impl GraphFight {
+    fn new(x_range: (f32, f32), y_range: (f32, f32), obstacles: u32, obstacle_size: f32) -> GraphFight {
+        utils::set_panic_hook();
+
+        if x_range.0 >= x_range.1 {
+            panic!("The first value of the x-range tuple should be smaller than the second value");
+        }
+
+        if y_range.0 >= y_range.1 {
+            panic!("The first value of the y-range tuple should be smaller than the second value");
+        }
+
+        let mut playerA = Vec::new();
+        let mut playerB = Vec::new();
+        let mut obstacles = Vec::new();
+
+        GraphFight {
+            playerA,
+            playerB,
+            obstacles,
+            x_range,
+            y_range
+        }
+    }
+}
+
