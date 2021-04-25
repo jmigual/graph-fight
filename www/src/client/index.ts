6d2435815409910968909361c02f55d7935f388b
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2021-04-25 21:20:00 +0200

    Start drawing

--- www/src/client/index.ts
+++ www/src/client/index.ts
@@ -1,15 +1,11 @@
 import * as wasm from "graph-fight";
 
+let game = new wasm.Game(20, 10, 2, .2, 4, 4, .05);
+window.requestAnimationFrame(draw);
+
 function draw(timestamp: number) {
     let canvas = <HTMLCanvasElement> document.getElementById('arena');
 
-    // game.draw(canvas);
+    game.draw(canvas);
     window.requestAnimationFrame(draw);
 }
-
-try {
-    let game = new wasm.Game(20, 10, 2, .2, 4, 4, .05);
-} catch (e) {
-    console.error("Error when creating game:", e);
-}
-window.requestAnimationFrame(draw);
