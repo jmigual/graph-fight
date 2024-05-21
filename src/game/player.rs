68fac7bab38adecaef416a7e174edf1a3ebf7dab
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2024-05-21 18:35:57 +0200

    Clean

--- src/game/player.rs
+++ src/game/player.rs
@@ -1,31 +1,8 @@
 use crate::geometry::*;
-use crate::utils;
 use serde::Serialize;
 use tsify::Tsify;
 use wasm_bindgen::prelude::*;
 
-pub mod style {
-    pub mod colour {
-        pub const TEAM_0: &str = "#F00";
-        pub const TEAM_1: &str = "#00F";
-        pub const TEAM_2: &str = "#0F0";
-        pub const TEAM_3: &str = "#f6ff52";
-        pub const TEAM_4: &str = "#F0F";
-        pub const TEAM_5: &str = "#0FF";
-        pub const TEAM_6: &str = "#A4C639";
-        pub const TEAM_7: &str = "#ffaf19";
-        pub const TEAM_8: &str = "#DFFF00";
-        pub const TEAM_9: &str = "#b219ff";
-
-        pub const TEAMS: &[&'static str] = &[
-            TEAM_0, TEAM_1, TEAM_2, TEAM_3, TEAM_4, TEAM_5, TEAM_6, TEAM_7, TEAM_8, TEAM_9,
-        ];
-
-        pub fn get_team_colour(idx: usize) -> &'static str {
-            TEAMS[idx % TEAMS.len()]
-        }
-    }
-}
 
 #[derive(Clone, Serialize,Tsify)]
 #[tsify(into_wasm_abi)]
